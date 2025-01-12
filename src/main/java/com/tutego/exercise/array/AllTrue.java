590ca373c7344419edaafe26eb5d1a71aa384865
Mr.Moon <MrMoonKr@gmail.com>
2025-01-12 14:27:42 +0900

    VS Code 개발 환경 및 java-formatter.xml 설정 추가 ...

--- src/main/java/com/tutego/exercise/array/AllTrue.java
+++ src/main/java/com/tutego/exercise/array/AllTrue.java
@@ -2,21 +2,21 @@ package com.tutego.exercise.array;
 
 public class AllTrue {
 
-  //tag::solution[]
-  private static boolean allTrue( boolean first, boolean... remaining ) {
+    // tag::solution[]
+    private static boolean allTrue( boolean first, boolean... remaining ) {
 
-    for ( boolean b : remaining )
-      if ( b == false )
-        return false;
+        for ( boolean b : remaining )
+            if ( b == false )
+                return false;
 
-    return first;
-  }
-  //end::solution[]
+        return first;
+    }
+    // end::solution[]
 
-  public static void main( String[] args ) {
-    System.out.println( allTrue( true, true, true ) );
-    System.out.println( allTrue( true ) );
-    System.out.println( allTrue( true, false ) );
-    System.out.println( allTrue( true, null ) );
-  }
+    public static void main( String[] args ) {
+        System.out.println( allTrue( true, true, true ) );
+        System.out.println( allTrue( true ) );
+        System.out.println( allTrue( true, false ) );
+        System.out.println( allTrue( true, null ) );
+    }
 }
